fb41f967caf6ababddb14fcd505b3f055a8a33f0
Lukaszc97 <114765173+Lukaszc97@users.noreply.github.com>
2023-03-18 19:43:10 +0100

    Json

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+let intervalId;
+stopBtn.disabled = true;
+function startColorSwitching() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  intervalId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+  }, 1000);
+}
+
+function stopColorSwitching() {
+  clearInterval(intervalId);
+    startBtn.disabled = false; 
+    stopBtn.disabled = true;
+    
+}
+
+startBtn.addEventListener('click', startColorSwitching);
+stopBtn.addEventListener('click', stopColorSwitching);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
---
+++ src/js/02-timer.js
@@ -0,0 +1,76 @@
+const dateTimePicker = document.querySelector('#datetime-picker');
+const startButton = document.querySelector('[data-start]');
+const daysValue = document.querySelector('[data-days]');
+const hoursValue = document.querySelector('[data-hours]');
+const minutesValue = document.querySelector('[data-minutes]');
+const secondsValue = document.querySelector('[data-seconds]');
+
+let countdownInterval;
+
+startButton.addEventListener('click', () => {
+  const selectedDate = dateTimePicker._flatpickr.selectedDates[0];
+
+  
+  if (!selectedDate) {
+    window.alert('Please choose a date first.');
+    return;
+  }
+
+
+  if (selectedDate.getTime() <= Date.now()) {
+    window.alert('Please choose a date in the future.');
+    return;
+  }
+
+  
+  startButton.disabled = true;
+
+  
+  const remainingTime = selectedDate.getTime() - Date.now();
+
+  countdownInterval = setInterval(() => {
+    const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor(
+      (remainingTime % (1000 * 60 * 60)) / (1000 * 60)
+    );
+    const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+    daysValue.textContent = days.toString().padStart(2, '0');
+    hoursValue.textContent = hours.toString().padStart(2, '0');
+    minutesValue.textContent = minutes.toString().padStart(2, '0');
+    secondsValue.textContent = seconds.toString().padStart(2, '0');
+
+    if (remainingTime <= 0) {
+      clearInterval(countdownInterval);
+      startButton.disabled = false;
+    } else {
+      remainingTime -= 1000;
+    }
+  }, 1000);
+});
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
